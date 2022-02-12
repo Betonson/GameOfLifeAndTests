cea775808810eb5ca85dd5322ff4facc63c4155b
Betonson <Leonel3289@mail.ru>
2022-02-13 00:17:48 +0300

    TileNeighbours
    Fdded logic to TileNeighbours, now tile can interact with neighbouring tiles

--- GameOfLifeAndTests/Assets/Code/GridManager.cs
+++ GameOfLifeAndTests/Assets/Code/GridManager.cs
@@ -4,32 +4,70 @@ using UnityEngine;
 
 namespace GameOfLifeAndTests
 {
-	public class GridManager : MonoBehaviour
-	{
-		[SerializeField] private int _width, _height;
-		[SerializeField] private Tile _tilePrefab;
+    public class GridManager : MonoBehaviour
+    {
+        [SerializeField] private int _width, _height;
+        [SerializeField] private Tile _tilePrefab;
         [SerializeField] private Transform _mainCamera;
-        private Tile[,] _TileCollection;
+        [SerializeField] private string _survivalCurve;
+        private Tile[,] _tileCollection;
 
         private void Start()
         {
-            _TileCollection = new Tile[_width,_height];
-            GenerateGrid(_TileCollection);
+            _tileCollection = new Tile[_width, _height];
+            GenerateGrid();
+            FillNeighbours();
         }
 
-        void GenerateGrid(Tile[,] _TileCollection)
+        private void GenerateGrid()
         {
-			for (int x = 0; x < _width; x++)
+            for (int x = 0; x < _width; x++)
             {
-				for(int y = 0; y < _height; y++)
+                for (int y = 0; y < _height; y++)
                 {
-					var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
-					spawnedTile.name = $"Tile {x} {y}";
-                    _TileCollection[x,y] = spawnedTile;
+                    var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+                    spawnedTile.name = $"Tile {x} {y}";
+                    _tileCollection[x, y] = spawnedTile;
                 }
             }
-
-            _mainCamera.position = new Vector3((float)_width/2-0.5f,(float)_height/2-0.5f,-10);
+            _mainCamera.position = new Vector3((float) _width / 2 - 0.5f, (float) _height / 2 - 0.5f, -10);
         }
-	}
+        
+        private void FillNeighbours()
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _height; y++)
+                {
+                    if (x > 0)
+                    {
+                        _tileCollection[x-1, y].AddNeighbour(_tileCollection[x, y]);
+                    }
+                    if (x < _width-1)
+                    {
+                        _tileCollection[x+1, y].AddNeighbour(_tileCollection[x, y]);
+                    }
+                    if (y > 0)
+                    {
+                        _tileCollection[x, y-1].AddNeighbour(_tileCollection[x, y]);
+                    }
+                    if (y < _height-1)
+                    {
+                        _tileCollection[x, y+1].AddNeighbour(_tileCollection[x, y]);
+                    }
+                }
+            }
+        }
+
+        private void CalculateNewStates()
+        {
+            for (int x = 0; x < _width; x++)
+            {
+                for (int y = 0; y < _height; y++)
+                {
+                    _tileCollection[x, y].CalculateNewState(_survivalCurve);
+                }
+            }
+        }
+    }
 }
---
+++ GameOfLifeAndTests/Assets/Code/TileLogic/Tile.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameOfLifeAndTests
+{
+	public class Tile : MonoBehaviour
+	{
+		[SerializeField] private GameObject _highlight;
+		public Color aliveColor;
+		public Color deadColor;
+		private TileBaseState _currentState, _calculatedState;
+		public TileBaseState nextState;
+		public TileAliveState aliveState = new TileAliveState();
+		public TileDeadState deadState = new TileDeadState();
+		private List<Tile> _neighbours = new List<Tile>();
+
+		private void Awake() {
+			SwitchState(aliveState);
+		}
+		private void OnMouseEnter()
+        {
+			_highlight.SetActive(true);
+			foreach (var neighbour in _neighbours)
+			{
+				neighbour._highlight.SetActive(true);
+			}
+		}
+
+		private void OnMouseExit()
+		{
+			_highlight.SetActive(false);
+			foreach (var neighbour in _neighbours)
+			{
+				neighbour._highlight.SetActive(false);
+			}
+		}
+		private void OnMouseDown()
+		{
+			SwitchState(nextState);
+		}
+
+		public void SwitchState(TileBaseState newState)
+		{
+			Debug.Log("Switching state to: " + newState.ToString());
+			_currentState = newState;
+			newState.OnEnterState(this);
+		}
+
+		public bool IsAlive()
+		{
+			return _currentState == aliveState ? true : false;
+		}
+
+		public void AddNeighbour(Tile newTile)
+		{
+			_neighbours.Add(newTile);
+		}
+
+		public void CalculateNewState(string survivalCurve)
+		{
+			
+		}
+	}
+}
